20054c17ac119fbc6c2985c63757b8cf73617af3
ZelunSun <sunzelun90@gmail.com>
2018-02-22 00:32:15 +0800

    update shipment module

--- resources/views/admin/shipment/index.blade.php
+++ resources/views/admin/shipment/index.blade.php
@@ -91,7 +91,7 @@
 		                    <i class="icon-magnifier"></i>
 		                    <h3>{{ trans('brackets/admin-ui::admin.index.no_items') }}</h3>
 		                    <p>{{ trans('brackets/admin-ui::admin.index.try_changing_items') }}</p>
-                            <a class="btn btn-primary btn-spinner" href="{{ url('admin/shipments/create') }}" role="button"><i class="fa fa-plus"></i>&nbsp; {{ trans('admin.shipment.actions.create') }}</a>
+                            <a class="btn btn-primary btn-spinner" href="{{ url('admin/shipments/create') }}" role="button"><i class="fa fa-plus"></i>&nbsp; {{ trans('New Shipment') }}</a>
 	                    </div>
                     </div>
                 </div>
